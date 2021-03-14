a8fe762612e119622821b7c5d387587557d6b6ab
Jonathan <jonathan.navas9@hotmail.com>
2021-03-13 20:18:53 -0500

    Implementacion de historial de busqueda

--- index.js
+++ index.js
@@ -20,23 +20,36 @@ const main = async () => {
                 //buscar los lugares
                 const lugares = await busquedas.ciudad( termino );
                 const id = await listarLugares(lugares);
+
+                if(id==='0') continue;
+
                 //seleccionar el lugar
                 const lugarSel = lugares.find( l => l.id === id );
 
+                busquedas.agregarHistorial(lugarSel.nombre);
+
                 //clima
                 const clima = await busquedas.climarLugar(lugarSel.lat,lugarSel.lng);
-                console.log(clima);
                 //mostrar informacion del clima
 
                 console.log('\nInformacion de la ciudad\n'.green);
-                console.log('Ciudad: ',lugarSel.nombre);
+                console.log('Ciudad: '.green,lugarSel.nombre);
                 console.log('Lat: ',lugarSel.lat);
                 console.log('Lng: ',lugarSel.lng);
                 console.log('Temperatura: ',clima.temp);
                 console.log('Minima: ',clima.min);
                 console.log('Maxima: ',clima.max);
-                console.log('Como esta el clima: ',clima.desc);
-                break;
+                console.log('Como esta el clima: ',clima.desc.cyan);
+
+            break;
+            case 2:
+                busquedas.historialCapitalizado.forEach( (lugar, i) => {
+                    const idx = ` ${ i + 1 } `.green;
+                    console.log( ` ${ idx } ${ lugar } ` );
+                });
+
+
+            break;
         }
 
         if(opt !== 0) await pausa();
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,11 +1,15 @@
+const fs = require('fs');
 const axios = require('axios');
 
 class Busquedas{
 
     historial = [];
+    
+    dbPath = './DB/database.json';
 
     constructor(){
         //TODO: Leer db si existe
+        this.leerDB();
     }
 
     get paramsMapBox(){
@@ -22,6 +26,16 @@ class Busquedas{
             'lang': 'es',
             'units': 'metric'
         }
+    }
+
+    get historialCapitalizado(){
+        return this.historial.map( lugar => {
+            
+            let palabras = lugar.split(' ');
+            palabras = palabras.map( p => p[0].toUpperCase() + p.substring(1));
+            return palabras.join(' ');
+
+        });
     }
 
     async ciudad (lugar = ''){
@@ -73,6 +87,45 @@ class Busquedas{
         } catch (error) {
             console.log(error);
         }
+    }
+
+    agregarHistorial( lugar='' ){
+        
+        //TODO Prevenir duplicados
+
+        if(this.historial.includes( lugar.toLocaleLowerCase())){
+            return;
+        }
+
+        this.historial = this.historial.splice(0,5);
+
+        this.historial.unshift(lugar.toLocaleLowerCase());
+
+        //grabar en un archivo json
+
+        this.guardaDB();
+
+    }
+
+    guardaDB(){
+
+        const payload ={
+            historial: this.historial
+        }
+        
+        fs.writeFileSync( this.dbPath, JSON.stringify( payload ));
+    }
+
+    leerDB(){
+        
+        if(!fs.existsSync(this.dbPath)) return;
+
+        const info = fs.readFileSync(this.dbPath, { encoding: 'utf8' });
+
+        const data = JSON.parse( info );
+
+        this.historial = data.historial;
+
     }
 
 }
